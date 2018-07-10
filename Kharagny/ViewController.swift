258cecaed498e6e92f67f552c819d6e2e0d420c3
Mohamed Sherif <msahmed652@gmail.com>
2018-07-10 13:25:11 +0200

    Initial Commit

---
+++ Kharagny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kharagny
+//
+//  Created by Mohamed Sherif on 7/10/18.
+//  Copyright © 2018 Gorgov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
